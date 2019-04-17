ce0d7b8151caff30bb35f421045f04bbafdb4552
Guillaume MICHEL <guillaume.michel@orilla.fr>
2019-04-17 23:44:41 +0200

    Change default height and width for app

--- app/main.lisp
+++ app/main.lisp
@@ -1,7 +1,7 @@
 (in-package :cl-logo.app)
 
-(defun start(&key (width 512)
-                  (height 512)
+(defun start(&key (width 600)
+                  (height 600)
                   (bg-color '(255 255 255)))
   (set-sdl2-backend-as-default :width width
                                :height height
